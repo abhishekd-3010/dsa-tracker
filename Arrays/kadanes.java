ceeceb1aaafaa71e083f8a7a324ac6e35a52fafe
abhishekd-3010 <abhishekdharamkar06@gmail.com>
2025-08-02 12:38:06 +0530

    Create kadanes.java

---
+++ Arrays/kadanes.java
@@ -0,0 +1,20 @@
+public class Kadanes {
+    public static void Kadanes(int numbers[]){
+        int cs = 0;
+        int ms = Integer.MIN_VALUE;
+
+        for (int i=0;i<numbers.length;i++){
+            cs = cs + numbers[i];
+            if (cs < 0){
+                cs = 0;
+            }
+            ms = Math.max(cs,ms);
+        }
+        System.out.println("Max sum is: "+ms);
+    }
+    public static void main(String args[]){
+        int numbers[] = {-2,-3,4,-1,-2,1,5,-3};
+        Kadanes(numbers);
+    }
+    
+}
