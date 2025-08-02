82adf12ddf48051d679eea87969ce2f5ca72dda9
abhishekd-3010 <abhishekdharamkar06@gmail.com>
2025-08-02 22:39:40 +0530

    Create pow(x,n).java

---
+++ Arrays/pow(x,n).java
@@ -0,0 +1,20 @@
+public class Solution {
+    public double myPow(double x, int n) {
+        long N = n; // To handle Integer.MIN_VALUE
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+
+        double ans = 1.0;
+        while (N > 0) {
+            if (N % 2 == 1) {
+                ans *= x;
+            }
+            x *= x;
+            N /= 2;
+        }
+
+        return ans;
+    }
+}
